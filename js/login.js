599b3c060f04146858291e199aebea884f820ad4
GuilhermeMCavalcanti <guilhermecavalcanto05@gmail.com>
2026-06-08 22:55:14 -0300

    Consertando perfil.js

--- js/login.js
+++ js/login.js
@@ -197,7 +197,7 @@
 
       /* 5. Redirecionar conforme o cargo */
       if (perfil === 'AFILIADO') {
-        window.location.href = './perfil_afiliado.html';
+        window.location.href = './perfil.html';
       } else {
         window.location.href = './perfil.html';
       }
